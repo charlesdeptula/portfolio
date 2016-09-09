03f11d424b5a3136d0694435194718e8d7be1ddc
charlesdeptula <charlesdeptula@Charless-MacBook-Pro.local>
2016-09-08 20:03:21 -0600

    updated discriptions on web page

--- wp-content/themes/portfolio/section-web.php
+++ wp-content/themes/portfolio/section-web.php
@@ -10,7 +10,7 @@
 						<div class="col-sm-6">
 							<div class="web-text">
 								<h3 class="web-title">FGD Law</h3>
-								<p class="web-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et elementum mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas ultrices nec orci quis interdum.</p>
+								<p class="web-description">Law Firm Website for FGD Law located in Boston, MA. Web site contains content managed image slider that is ituitive to update in the back end.</p>
 								<a href="http://fgd-law.com" target="_blank">
 									<button class="main">Visit Site</button>
 								</a>
@@ -26,7 +26,7 @@
 						<div class="col-sm-6">
 							<div class="web-text">
 								<h3 class="web-title">Red Mtn Productions</h3>
-								<p class="web-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et elementum mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas ultrices nec orci quis interdum.</p>
+								<p class="web-description">Portfolio web site for Red Mtn Productions based in Aspen, CO. Site contains an easy to use backend where the user can post videos from their vimeo account just by adding a link, as well as add clients and update other content.</p>
 								<a href="http://redmtnproductions.com" target="_blank">
 									<button class="main">Visit Site</button>
 								</a>
@@ -42,7 +42,7 @@
 						<div class="col-sm-6">
 							<div class="web-text">
 								<h3 class="web-title">Swan Boats</h3>
-								<p class="web-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et elementum mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas ultrices nec orci quis interdum.</p>
+								<p class="web-description">Web Site for the Swan Boats located in the Boston Commons. Site contains a custom plugins that displays business's opened status. The user can also update the status in the backend.</p>
 								<a href="http://swanboats.com" target="_blank">
 									<button class="main">Visit Site</button>
 								</a>
@@ -58,7 +58,7 @@
 						<div class="col-sm-6">
 							<div class="web-text">
 								<h3 class="web-title">Elephas Creative</h3>
-								<p class="web-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla et elementum mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas ultrices nec orci quis interdum.</p>
+								<p class="web-description">Portfolio site for Elephas Creative located outside of Boston MA. Site contains a dynamic home page that randomly selects a series of background images to display from a database the user creates. Site also has an interactive map on the contact page/</p>
 								<a href="http://elephascreative.com" target="_blank">
 									<button class="main">Visit Site</button>
 								</a>
