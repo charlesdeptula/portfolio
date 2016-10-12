7c63cba56b86b0b423c7fb9c3cc741989adebb61
charlesdeptula <charlesdeptula@Charless-MacBook-Pro.local>
2016-10-12 11:37:56 -0700

    centered reel on reel page

--- wp-content/themes/portfolio/page-reel.php
+++ wp-content/themes/portfolio/page-reel.php
@@ -2,8 +2,10 @@
 
 get_header(); ?>
 
-<video class="reel" controls autoplay>
-  <source src="<?php bloginfo('stylesheet_directory'); ?>/media/2016-demoReelWide.mp4" type="video/mp4" />
-</video>
+<div class="reel-container">
+	<video class="reel" controls autoplay>
+	  <source src="<?php bloginfo('stylesheet_directory'); ?>/media/2016-demoReelWide.mp4" type="video/mp4" />
+	</video>
+</div>
 
 <?php get_footer(); ?>
